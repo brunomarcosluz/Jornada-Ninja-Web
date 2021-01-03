76086dffad656c1c4a214714127fb1685916708f
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-01-02 21:11:07 -0300

    Create organizacao.js

---
+++ fundamentos/organizacao.js
@@ -0,0 +1,35 @@
+// Como o Javascript é organizado?
+
+/* Ele é dividio em sentenças de códigos que pode terminar 
+com ; (ponto e vírgula) ou não. */
+
+//Exemplo de uma sentença:
+ console.log("Hello, World!");
+
+//Exemplo de blocos de código:
+
+{
+ console.log("Olá,Mundo!");
+ console.log("Olá, Humano!");
+}
+
+/* perceba que os blocos são fechados por chaves {} pode existir
+blocos dentro de outros blocos assim */
+
+{
+    console.log("Estou dentro de um bloco com um bloco dentro?");
+    {
+        console.log("Estou dentro de um bloco que está em outro bloco!");
+    }
+}
+
+/* As sentenças não se limitam dentro dos blocos! Podemos ter várias
+sentenças dentro de um único bloco! */
+
+{
+    console.log("sentença1");
+    console.log("sentença2");
+    console.log("ao infinito e além!");
+}
+
+/* -------------- Bruno Marcos Luz ------------ */
