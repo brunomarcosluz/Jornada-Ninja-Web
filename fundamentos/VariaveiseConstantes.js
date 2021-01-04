cd7733038336e03d8a95dd8bd576ca79aca87e4b
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-01-03 21:46:36 -0300

    commit

---
+++ fundamentos/VariaveiseConstantes.js
@@ -0,0 +1,21 @@
+// Nessa aula vi o básico sobre váriaveis e constantes em JS 
+
+/* Em todas as linguagens de programação existem existem palavras
+reservadas as estudadas hoje foram var, let e const..
+
+Sendo var e let palavrás usadas para declarar variáveis e const para constantes.
+
+Exemplos de declarações: */
+
+var nome = "Bruno"
+let jogofav = "Red Dead Redemption"
+const Jesus = "É top"
+
+console.log(nome)
+console.log(jogofav)
+console.log(Jesus)
+
+/* declare-se primeiro a palavra reservada em seguida o nome da variável ou constante
+e depois o seu valor! */
+
+//---------------- Bruno Marcos Luz de Macedo ----------------//
