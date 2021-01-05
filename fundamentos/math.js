cf85c47e92bffb8ddf677a5309e99dda43234c26
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-01-05 15:36:27 -0300

    commit

---
+++ fundamentos/math.js
@@ -0,0 +1,12 @@
+// Usando o objeto Math
+
+/* Em JS nos usamos o objeto Math para facilitar os nossos cálculos
+como exemplo vamos descobrir um diâmetro de uma área */
+
+const raio = 5.6
+const area = Math.PI * Math.pow(raio,2)
+
+console.log(area);
+
+// a função typeof é para descobrir qual é o tipo do valor informado
+console.log(typeof Math);
