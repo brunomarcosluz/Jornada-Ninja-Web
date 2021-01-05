a33222dbb2af77529db840b60cd82e31680a55b3
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-01-04 23:16:46 -0300

    commit

---
+++ fundamentos/TipagemFraca.js
@@ -0,0 +1,19 @@
+// Javascript a linguagem indecisa
+
+/* o Javascript é uma linguagem fracamente tipada, ou seja uma variável
+não precisa necessáriamente de um TIPO definido
+
+Exemplo variável Jogo Favorito: */
+
+var JogoFav = "Red Dead Redemption"
+
+console.log(JogoFav);
+
+var JogoFav = 1
+
+console.log(JogoFav);
+
+/* aqui a variável Jogo Favorito recebeu um texto como o esperado mas logo depois o programador
+no caso eu declarei ela novamente atribuindo um novo valor e ela assumiu-se como tipo INT*/
+
+//-------------Bruno Marcos Luz de Macedo-------------------//
