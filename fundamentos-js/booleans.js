c0171c310761119e15cbe834495924a4db17c966
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-01-06 19:59:17 -0300

    commit

---
+++ fundamentos-js/booleans.js
@@ -0,0 +1,17 @@
+// tratando dados booleanos em Javascript
+
+let Ativo = false;
+
+if(Ativo == true){
+    console.log("Está ligado");
+}
+else{
+    console.log("Está desligado");
+}
+
+console.log(Ativo);
+
+
+//usando a operação || (Ou)
+var nome = 'Bruno'
+console.log(nome || 'Desconhecido');
