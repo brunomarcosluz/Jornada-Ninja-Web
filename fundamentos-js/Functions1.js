e1630d04c1ba5e83ba44b2a3d85c95a64476a14b
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-01-08 19:56:01 -0300

    Create Functions1.js

---
+++ fundamentos-js/Functions1.js
@@ -0,0 +1,16 @@
+// Exemplos Básicos sobre Funções 
+
+function imprimirSoma(n1, n2){ //função sem retorno
+    console.log(n1 + n2);
+}
+imprimirSoma(2, 2)
+
+function soma(numero1, numero2) { // função com retorno (return)
+    return (numero1 + numero2)
+}
+console.log(soma(4, 4));
+
+/* é bem simples declaramos a função com a palavra reservada (function) e em seguida passamos os
+devidos parâmetros. */
+
+//----------------@brunomarcosluz----------------------//
