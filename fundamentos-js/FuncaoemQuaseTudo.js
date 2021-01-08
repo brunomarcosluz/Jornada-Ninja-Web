1e5632afa092b30fa9684266ecd3278fbb8c6051
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-01-08 19:10:24 -0300

    Create FuncaoemQuaseTudo.js

---
+++ fundamentos-js/FuncaoemQuaseTudo.js
@@ -0,0 +1,12 @@
+/* "Se o javascript fosse um país o presidente seria a função!" 
+
+ A função no javascript é algo quase que fundamental, se você entende função
+ você entende javascript e vice-versa.*/
+
+ console.log(typeof Object); // perceba que o Objeto em JS pe uma função!
+
+ class prod1 {}
+
+ console.log(typeof prod1); // e a classe prod1 também é uma função!!
+
+ 
