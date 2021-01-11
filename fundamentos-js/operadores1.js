3eb69382dbc0d5ccec646a39db00e57bcb1cb771
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-01-11 20:25:57 -0300

    commit

---
+++ fundamentos-js/operadores1.js
@@ -0,0 +1,21 @@
+// Operadores: Atribuição
+
+/* A atribuição em Javascript se assemelha com as demais 
+linguagens, sem muito mistério: */
+
+var b = 4 // (=) sinal de atribuição
+
+// podemos fazer operações com o sinal de atribuição:
+
+let a = 2
+
+b += a // seria o mesmo de b = b + a
+console.log(b);
+b -= a // b = b - a
+console.log(b);
+b *= a
+console.log(b);
+b /= a
+console.log(b);
+b %= 2
+console.log(b);
