bd7de6e2e230d0aac96497cc2ce3fc9d56d3b2eb
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-01-10 23:09:53 -0300

    commit

---
+++ fundamentos-js/funcaoVSobjeto.js
@@ -0,0 +1,20 @@
+// Função vs Objeto
+
+/* Se javascript fosse uma república a função seria o presidente
+esse conceito tem que ficar gravado em nossa mente já que quase tudo
+é uma função em JS até mesmo classes! 
+
+Observe os exemplos e suas saídas: */
+
+console.log(typeof Object); // saída = function
+console.log(typeof new Object); // saída = Object por conta do (new)
+
+class Product {}
+console.log(typeof Product); // saída = function?
+
+/* As classes são funções em JS, o seu nome popular é sugar sintax
+como se fosse um ebelezador de código, instanciamos uma função.
+
+Estudaremos funções mais a fundo no capítulo de funções */
+
+//------------------@brunomarcosluz-----------------//
