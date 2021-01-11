09ce2b3b9f6ba1a83f9f31d73aae68aee3eb3f4b
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-01-11 16:55:40 -0300

    commit

---
+++ fundamentos-js/notacaoPonto.js
@@ -0,0 +1,12 @@
+// breve estudo sobre notação ponto
+
+/* Notação ponto esteve presente durate todos os nossos estudos,
+sua função é instanciar e referênciar o que queremos
+
+exemplos: */
+
+console.log(Math.ceil(6.7)); // duas notações ponto em uma única sentença
+
+console.log("Notação Ponto é top"); // console . log (ponto)
+
+//--------------@brunomarcosluz------------//
