f2134ebc268a559113a86a1252de2f7a728d1c53
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-01-12 18:34:36 -0300

    commit

---
+++ fundamentos-js/Destructuring2.js
@@ -0,0 +1,12 @@
+// O Destructuring pode desestruturar não apenas objetos mas arrays também
+
+const [a] = [10] // agora temos uma váriavel a com o valor de 10
+
+console.log(a);
+
+// uma maneira super rápida para se criar várias variaveis
+
+const [b,c,d,e] = [2,4,8,12] // Destructuring é bom d+
+console.log(b,c,d,e);
+
+//---------@brunomarcosluz------------//
