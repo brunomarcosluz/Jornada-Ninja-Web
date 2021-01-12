b155a4a27eef0408ec57e5553c9fa43bb18bddc7
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-01-12 18:47:04 -0300

    commit

---
+++ fundamentos-js/operadoresAritmeticos.js
@@ -0,0 +1,12 @@
+// Simples e fundamental
+
+/* Operações aritméticas são tão naturais que esquecemos de estuda-las
+aqui está um estudo sobre como faze-las em javascript */
+
+/* Operações aritméticas são conhecidas por serem binárias
+pois tem dois operadores exemplo: a + b sendo a o primeiro operador
+e b o segundo  */
+
+const [a,b,c,d] = [1,2,3,4] // Declarando as variáveis com Destructuring
+
+const soma = a + b + c + d 
