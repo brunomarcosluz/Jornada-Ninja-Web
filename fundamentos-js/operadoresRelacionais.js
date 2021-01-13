38e1f93ee503a27944a8f3f713aa5dc74d1236fe
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-01-13 15:56:08 -0300

    commit

---
+++ fundamentos-js/operadoresRelacionais.js
@@ -0,0 +1,21 @@
+// Estudo sobre Operadores Relacionais e como se comportam em JS
+
+
+/* Operadores relacionais sempre irão retornar o valor
+true ou false ou seja, sempre vão retornar se é 
+verdadeiro ou falso seguimos aos exemplo: */
+
+var a = 1
+var b = '1'
+
+console.log(a == b); // variável a é igual a b? true
+console.log(a === b); // variável a é estritamente igual a b? false
+
+/* São os operadores relacionais: */
+
+console.log(a<b); // menor que
+console.log(a>b); // maior que
+console.log(a<=b); // menor ou igual
+console.log(a>=b); // maior ou igual
+
+//--------@brunomarcosluz-------------//
