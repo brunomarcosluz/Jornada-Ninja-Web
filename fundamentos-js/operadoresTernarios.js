96fbfd96484ed6e56b9fc8d1666d56c45a3fb183
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-01-17 00:06:03 -0300

    commit

--- fundamentos-js/operadoresTernarios.js
+++ fundamentos-js/operadoresTernarios.js
@@ -1,3 +1,9 @@
 // Estudos sobre Ternários e como se comportam em JS
 
-/*  */
+/* Como seu nome já diz os operadores Ternários são identificados
+quando existem três operandos exemplo: */
+
+const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado'
+
+console.log(resultado(8));
+console.log(resultado(5));
