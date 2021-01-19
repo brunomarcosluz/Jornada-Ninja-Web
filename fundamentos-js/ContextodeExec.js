bdabb0fc4707fa385a4a91658eb842772cc7df8a
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-01-18 23:05:49 -0300

    commit

--- fundamentos-js/ContextodeExec.js
+++ fundamentos-js/ContextodeExec.js
@@ -1,3 +1,12 @@
 // Contexto de execução Browser vs Node
 
-/* */
+/* Esta aula foi baseada e serviu como um
+reforçodo que o professor vem comentando ao longo
+do curso o contexto global e o objeto window */
+
+let a = 3 
+globalThis.b = 123
+this.c = 456
+
+console.log(this.a);
+console.log(global.b);
