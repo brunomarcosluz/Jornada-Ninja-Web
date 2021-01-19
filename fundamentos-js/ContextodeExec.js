13b8367be99308b4fe29fc57a63f6e143854ac9b
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-01-18 23:08:39 -0300

    commit

--- fundamentos-js/ContextodeExec.js
+++ fundamentos-js/ContextodeExec.js
@@ -4,6 +4,9 @@
 reforçodo que o professor vem comentando ao longo
 do curso o contexto global e o objeto window */
 
+// dentro do Node cada arquivo é um módulo diferente
+// nossa aplicação fica toda modularizada
+
 let a = 3 
 globalThis.b = 123
 this.c = 456
