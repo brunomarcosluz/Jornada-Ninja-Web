4cae29a949e4875f19da4af6799c7dc14082b724
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-01-21 18:56:00 -0300

    commit

---
+++ estruturas de controle/if1.js
@@ -0,0 +1,14 @@
+// Estudo sobre a palavra reservada if
+
+/* If(){} é uma estrutura de controle onde podemos
+colocar uma condição em nosso código
+
+exemplo: */
+
+function notaAluno(nota){
+    if (nota > 5) {
+        return console.log('Aprovado com nota ' + nota);        
+    }
+}
+
+notaAluno(6)
