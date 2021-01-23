0274542d808261eabaa781afdea89fe5ff547938
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-01-22 22:42:25 -0300

    commit

---
+++ estruturas de controle/ifElse.js
@@ -0,0 +1,15 @@
+// estudos sobre a estrutura completa If + Else
+
+/* Else funciona como um último recurso caso o If não retorne falso
+exemplo: */
+
+const imprimirResultado = function(nota){
+    if(nota >= 7){
+        console.log("Aprovado, parabéns!");
+    } else {
+        console.log("Reprovado, se esforçe mais da próxima vez");
+    }
+}
+
+imprimirResultado(10)
+imprimirResultado(3)
