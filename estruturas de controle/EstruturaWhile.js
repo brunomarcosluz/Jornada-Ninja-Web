4682893c30df0b02c18c128a90a2465dc84c97d1
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-01-25 20:57:56 -0300

    commit

--- estruturas de controle/EstruturaWhile.js
+++ estruturas de controle/EstruturaWhile.js
@@ -1,3 +1,6 @@
 // Estudos sobre a estrutura While em Javascript 
 
-/* O While é a primeira estrutura de controle  */
+/* O While é a primeira estrutura de controle que vemos que 
+se caracteriza como Laço de Repetição
+
+O While = Enquanto */
