2ba646a776fb4757e64ffcc78425e2db3d8f1434
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-01-26 20:09:11 -0300

    commit

--- estruturas de controle/EstruturaWhile.js
+++ estruturas de controle/EstruturaWhile.js
@@ -3,4 +3,20 @@
 /* O While é a primeira estrutura de controle que vemos que 
 se caracteriza como Laço de Repetição
 
-While = Enquanto */
+While = Enquanto 
+
+Assim como o if o While retorna um valor booleano (verdadeiro ou falso) */
+
+function getIntAleatorioEntre (min, max){
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+}
+
+let opcao = 0
+
+while (opcao != -1) {
+    opcao = getIntAleatorioEntre(-1, 10)
+    console.log(`Número sorteado foi: ${opcao}!`);
+}
+
+console.log('Até a próxima');
