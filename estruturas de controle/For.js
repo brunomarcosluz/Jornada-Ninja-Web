3b740f03b3ef42c7c5599d0dd9e597cbf057c551
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-01-27 18:44:57 -0300

    commit

---
+++ estruturas de controle/For.js
@@ -0,0 +1,8 @@
+// Estudo sobre a palavra reservada For em JS 27/01/2021
+
+/* O for diferente do while é um pouco mais usado
+por conta da sua simplicidade e que com ele podemos declarar
+exatamente quantas repetições queremos 
+
+observe:  */
+
