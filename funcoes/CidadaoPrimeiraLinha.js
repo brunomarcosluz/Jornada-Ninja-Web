dbdf6ee88b951f72a1d60c0ff82dadc555665f25
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-01-29 20:12:29 -0300

    commit

--- funcoes/CidadaoPrimeiraLinha.js
+++ funcoes/CidadaoPrimeiraLinha.js
@@ -26,3 +26,17 @@ obj.falar = function(){return 'Eaeee!'}
 console.log(obj.falar());
 
 // podemos passar uma função como parâmetro de outra função:
+
+function run(fun){
+    fun()
+}
+run(function(){console.log('Executando função')})
+
+// uma função pode retornar/conter outra função:
+
+function soma(a,b) {
+    return function(c){
+        console.log(a + b + c);
+    }    
+}
+soma(2,3)(4) // o (4) é a variável (c)
