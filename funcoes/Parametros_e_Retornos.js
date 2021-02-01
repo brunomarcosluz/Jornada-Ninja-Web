4c9c18c5488ffb5fbd9c24706a93fd83e399fa99
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-02-01 19:59:49 -0300

    commit

--- funcoes/Parametros_e_Retornos.js
+++ funcoes/Parametros_e_Retornos.js
@@ -1,3 +1,21 @@
 // Estudos sobre parâmetros e retornos em Javascript
 
-/* Parâmetros e retornos em javascript são opcionais */
+/* Parâmetros e retornos em javascript são opcionais, isso nos 
+da uma flexibilidade grande na linguagem, podemos escrever diferentes
+padrões ou fazer grandes cagadas
+
+aqui em baixo vou mostrar um exemplo bem maluco
+de uma função que não é válida na prática */
+
+function area(largura, altura) {
+    const area= largura * altura
+    if (area > 20){
+        console.log(`Acima do valor permitido: ${area}m2.`); // perceba que aqui eu não usei o return
+    } else {
+        return area
+    }
+}
+
+console.log(area(5,5));
+console.log(area());
+
