f73480ed9373b25c50f267acfb725ada4b7654e1
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-02-01 22:16:16 -0300

    commit

---
+++ exercicios-js/exec1.js
@@ -0,0 +1,17 @@
+// Fundamentos de Pogramação com Javascript
+
+// primeiro exercício :
+
+/* Crie uma função que retorna string "Olá", concatenada com um 
+argumento text (a ser passado para a função) e com ponto de exclamação "!" 
+no final. 
+
+Exemplos: 
+
+coomprimentar("Fulano") // retornará "Olá, fulano!" */
+
+function oi(nome) {
+    console.log(`Olá, ${nome}!`);    
+}
+
+oi("Bruno");
