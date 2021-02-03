caf338aa9600d532cf828edca7b7c4104d2f140f
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-02-02 22:35:34 -0300

    commit

--- funcoes/ParametrosPadroes.js
+++ funcoes/ParametrosPadroes.js
@@ -14,3 +14,20 @@ function soma(a,b,c){ // forma antiga mais ainda utilizada para fazer parâmetro
 }
 
 console.log(soma(), soma(2)); 
+/* Essa forma de fazer param padrões pode gerar um erro, quando passado o 
+valor 0 para a função o operador que implementamos retornará falso fazendo com
+que o parâmetro padrão não seja atualizado. */
+
+console.log(soma(0,0,0)); // saída = 3 (bug)
+
+// Nova forma ECMAscript de se realizar parâmetros padrões:
+
+function soma2(a=1,b=1,c=1){
+    return a + b + c
+}
+
+console.log(soma2()); 
+
+// a nova forma é mais rápida simples e direto ao ponto
+
+// @brunomarcosluz
