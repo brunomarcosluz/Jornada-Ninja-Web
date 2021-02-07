394f3980dfbfb921bc8fc321ea9a73f06ccc2f7c
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-02-07 20:56:23 -0300

    commit

--- exercicios-js/fundamentos-estruturas-e-funcoes/exercicio1.js
+++ exercicios-js/fundamentos-estruturas-e-funcoes/exercicio1.js
@@ -1,6 +1,6 @@
 // Curso Web Moderno escola Cod3r 
 
-/* Crie uma função que dado dois valores (passados como parâmetros) 
+/* 1-) Crie uma função que dado dois valores (passados como parâmetros) 
 mostre no console a soma, subtração, multiplicação e divisão desses valores */
 
 function matematica(n1,n2) {
