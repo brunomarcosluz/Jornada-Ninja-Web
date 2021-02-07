f149013c08e4eac58c63ba378940d3cfbf548be9
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-02-07 20:54:50 -0300

    commit

---
+++ exercicios-js/fundamentos-estruturas-e-funcoes/exercicio1.js
@@ -0,0 +1,10 @@
+// Curso Web Moderno escola Cod3r 
+
+/* Crie uma função que dado dois valores (passados como parâmetros) 
+mostre no console a soma, subtração, multiplicação e divisão desses valores */
+
+function matematica(n1,n2) {
+    return console.log("soma: \n", n1 + n2,"subtração: \n", n1 - n2,"multiplicação: \n", n1 * n2, "divisão: \n", n1 / n2,);
+}
+
+matematica(2,2)
