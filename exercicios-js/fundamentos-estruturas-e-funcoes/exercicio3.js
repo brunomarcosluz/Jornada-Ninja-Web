8de8e56adbbe864c5966a2b08b0f8cf03f949d73
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-02-08 21:50:16 -0300

    commit

---
+++ exercicios-js/fundamentos-estruturas-e-funcoes/exercicio3.js
@@ -0,0 +1,11 @@
+// Exercício 3 
+
+/* Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base
+elevada ao expoente: */
+
+ElevaPainho = (base, expoente) => {
+    return (base**expoente)    
+}
+
+console.log(ElevaPainho(2,3));
+console.log(ElevaPainho(4,4));
