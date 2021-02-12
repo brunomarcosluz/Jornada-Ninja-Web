50f488a457396d9c4d7c5c47125c67953a301ee7
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-02-12 19:28:47 -0300

    Create exercicio4.js

---
+++ exercicios-js/fundamentos-estruturas-e-funcoes/exercicio4.js
@@ -0,0 +1,12 @@
+// Quarto exercício da lista, objetivo: criar uma função que retorne o módulo (resto) da divisão
+
+/* 4-) Crie uma função que irá receber dois valores, o dividendo e o divisor. A função deverá imprimir o resultado
+e o resto da divisão destes dois valores.
+*/
+
+function restoDe(dividendo,divisor) {
+    console.log(`o resto de ${dividendo} e ${divisor} é:`, dividendo % divisor);
+}
+
+restoDe(8,3);
+restoDe(10,2);
