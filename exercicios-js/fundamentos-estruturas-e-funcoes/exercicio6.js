1a8cbd15bf33a282a0bacc50f2703ab2d7032189
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-02-14 13:29:44 -0300

    commit

---
+++ exercicios-js/fundamentos-estruturas-e-funcoes/exercicio6.js
@@ -0,0 +1,20 @@
+// Seja bem vindo ao sexto desafio javascript!
+
+/* 06) Elabore duas funções que recebem três parâmetros: capital inicial, taxa de juros e tempo de aplicação. A
+primeira função retornará o montante da aplicação financeira sob o regime de juros simples e a segunda
+retornará o valor da aplicação sob o regime de juros compostos */
+
+// C = Capital, i = taxa de juros, t = tempo de aplicação
+function jurosSimples(C,i,t) {
+    let montante 
+    const juros = 200
+    montante = C + juros
+    t = juros / C
+    return console.log("montante= ",montante, "\ntaxa de juros= ", t,'%');
+}
+
+jurosSimples(1000)
+
+function jurosCompostos(C,i,t){
+    let montante
+}
