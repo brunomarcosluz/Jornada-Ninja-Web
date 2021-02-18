d397e9616b40dc4e2bcdbb2f5c9cbdde278dd1f5
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-02-18 18:49:32 -0300

    commit

--- exercicios-js/fundamentos-geral/experimento1.js
+++ exercicios-js/fundamentos-geral/experimento1.js
@@ -1,7 +1,7 @@
 // Primeiro experimento aplicando conceitos estudados
 
-/* Este experimento em geral aborda o tema estudado sobre
-parâmetros padrões, onde gostaria de coloco um teste lógico sobre ele */
+/* Este experimento aborda o tema parâmetros padrões,
+onde coloco um teste lógico sob o parâmetro padrão */
 
 function soma(a=1,b=1,c=1) {
     if (a==0,b==0,c==0){
