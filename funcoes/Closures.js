216a9ea851a89afcc14f6e933f77591a23525651
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-02-17 21:22:28 -0300

    commit

--- funcoes/Closures.js
+++ funcoes/Closures.js
@@ -1,1 +1,19 @@
 // estudo sobre Closures
+
+/* Closures não é nada mais do que o contexto léxico em ação,
+para saber mais sobre contexto léxico visite o arquivp ContextoLexico.js
+
+para facilitar:
+
+Closure = contexto léxico no uqal o código está inserido. */
+
+const x = 'global'
+
+function fora(){
+    const x = 'local'
+    function dentro() {
+        return x;        
+    }
+    return dentro;
+}
+
