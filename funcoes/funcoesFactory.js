87a48ff8d8cc7b20e45a6392f4d81f623ad5aea6
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-02-21 19:46:24 -0300

    commit

--- funcoes/funcoesFactory.js
+++ funcoes/funcoesFactory.js
@@ -1,3 +1,16 @@
 // estudo sobre funções factory
 
-/* parte 1 */
+/* Funções factory ou funções "fábricas" basicamente retornam
+uma instância de um novo objeto
+
+exemplo com código: função criarPessoa */
+
+function criarPessoa() {
+    return {
+        nome: 'Bruno',
+        sobrenome: ' Marcos'
+    }
+}
+
+console.log(criarPessoa());
+
