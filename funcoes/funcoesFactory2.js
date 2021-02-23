3d34c932413b330f6664cc148451ba878376ff89
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-02-22 21:00:13 -0300

    commit

--- funcoes/funcoesFactory2.js
+++ funcoes/funcoesFactory2.js
@@ -11,7 +11,21 @@ function criarProduto (produto,preco){
         desconto: 0.1
     }
 }
-
+console.log("=== CRIANDO PRODUTOS ===");
 console.log(criarProduto("Mac Book",10000));
 console.log(criarProduto("PS5", 5000));
 console.log(criarProduto("Você", "impagável"));
+
+console.log("=========================================");
+// factory funciton (criarAnimal)
+
+function criarAnimal (nome,especie){
+    return {
+        nome,
+        especie
+    }
+}
+console.log("=== CRIANDO ANIMAIS ===");
+console.log(criarAnimal("Geraldo", "Tamanduá"));
+console.log(criarAnimal("Denis", "Mico-Leão-Dourado"));
+console.log(criarAnimal("Nino","Cachorro"));
