aa209dbcd892586da70fd3bdbba6f2db51deba1b
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-02-23 22:31:41 -0300

    commit

--- funcoes/classeVsFactory.js
+++ funcoes/classeVsFactory.js
@@ -6,9 +6,20 @@ class Pessoa {
     }
     
     falar() {
-        console.log(`Meu nome é ${this.nome}`)
+        console.log(`Hi my name is ${this.nome}`)
     }
 }
 
 const p1 = new Pessoa("Bruno")
 p1.falar()
+
+// Função Factory
+
+const Pessoa2 = nome => {
+    return {
+        falar: () => console.log(`Hi Bruno my name is ${nome}`)
+    }
+}
+
+const p2 = Pessoa2("Bernard")
+p2.falar()
