b7b0a4425f36f3d28e20c07854b207e978b4fcc9
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-02-24 21:13:10 -0300

    commit

--- funcoes/IIFE.js
+++ funcoes/IIFE.js
@@ -3,5 +3,12 @@
 Ou funções imediatamente invocadas.
 
 Resumidamente são funções dentro de () que são 
-executadas logo após a sua declaração */
+executadas logo após a sua declaração, além de fugir do escopo
+global que é uma boa! */
 
+(function() {
+    console.log('É executado na hora!')
+    console.log('Foge do escopo global!')
+})()
+
+// @brunomarcosluz
