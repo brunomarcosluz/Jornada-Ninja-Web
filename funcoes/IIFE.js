4a9b783d847ba10c9b5c26b2e9e9a9c354043a7b
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-02-24 21:05:52 -0300

    commit

---
+++ funcoes/IIFE.js
@@ -0,0 +1,7 @@
+/* IIFE = Immediately Invoked Function Expression 
+
+Ou funções imediatamente invocadas.
+
+Resumidamente são funções dentro de () que são 
+executadas logo após a sua declaração */
+
