ef5f6a3f579e46b912b1a6cb2138ccefc875b4a3
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-02-27 19:21:39 -0300

    commit

--- objetos-js/introdOO.js
+++ objetos-js/introdOO.js
@@ -1,1 +1,8 @@
 // Introdução a Orientação a Objetos
+
+const Bruno = {
+    nomeCompleto: 'Bruno Marcos Luz de Macedo',
+    graduacao: 'Ciência da Computação 5/8',
+    linguagens: ['Javascript', 'Java', 'PHP', 'C'],
+    idiomas: ['Inglês', 'Português-BR', 'Francês Básico']
+}
