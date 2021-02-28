02dccb33633dc483efbabacf780a116ee8855368
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-02-28 11:55:22 -0300

    commit

--- objetos-js/introdOO.js
+++ objetos-js/introdOO.js
@@ -1,8 +1,20 @@
 // Introdução a Orientação a Objetos
 
+/* Orientação a Objetos é um paradigma muito importate para a história
+da computação e como microcomputadores eram programados, a ideia principal do 
+paradigma é abstrair coisas físicas ou não do mundo real e transformá-las em
+cóigo fonte.
+
+Observe a seguir como eu Me transformei em um Objeto colocando poucas 
+características minhas como atributos: */
+
 const Bruno = {
     nomeCompleto: 'Bruno Marcos Luz de Macedo',
     graduacao: 'Ciência da Computação 5/8',
     linguagens: ['Javascript', 'Java', 'PHP', 'C'],
     idiomas: ['Inglês', 'Português-BR', 'Francês Básico']
 }
+
+console.log(Bruno.graduacao)
+console.log(Bruno.idiomas)
+console.log(Bruno.linguagens)
