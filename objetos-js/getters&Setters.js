8477155dbdbbf36a3b0563e95635fe482f045cc0
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-03-04 20:55:35 -0300

    commit

--- objetos-js/getters&Setters.js
+++ objetos-js/getters&Setters.js
@@ -1,14 +1,18 @@
 // estudo sobre get e set em javascript
 
+/* Esses métodos nos garantem encapsulamento em um bloco de código
+você pode ver como o objeto sequência foi estruturado, eu não
+posso acessar o atributo _valor e alteralo sem o método get */
+
 
 const sequencia = {
     _valor: 1,
-    get valor() {
-        return this.valor++
-    },
+    get valor() {return this._valor++},
     set valor(valor) {
         if(valor > this._valor){
             this._valor = valor
         }
     }
 }
+
+console.log(sequencia.valor, sequencia.valor);
