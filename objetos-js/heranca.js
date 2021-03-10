6a903351b550d13f5601e146fd2bbfe5cbf3b831
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-03-10 07:47:45 -0300

    commit

--- objetos-js/heranca.js
+++ objetos-js/heranca.js
@@ -1,5 +1,13 @@
 // introdução a herança parte fundamental da orientação de objetos
 
 /* Herança assim como seu nome propriamente diz, é quando se herda 
-atributos de uma classe pai (ou mãe) */
+atributos de uma classe pai (ou mãe)
 
+Em javascript herança se da pelo atributo [[prototype]] */
+
+const BMW = {
+    modelo: '320i',
+    velMax: 340
+}
+
+console.log(BMW.__proto__);
