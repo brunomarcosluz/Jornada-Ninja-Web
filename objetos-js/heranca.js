a42164c4ef213b65a52540364ef503af7e091f5b
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-03-28 14:10:06 -0300

    commit

--- objetos-js/heranca.js
+++ objetos-js/heranca.js
@@ -5,9 +5,27 @@ atributos de uma classe pai (ou mãe)
 
 Em javascript herança se da pelo atributo [[prototype]] */
 
-const BMW = {
-    modelo: '320i',
-    velMax: 240
+const CarroLuxo = {
+    velMax: '240Km/h',
+    qtdRodas: 4,
+    arCondicionado: true,
+    vidroEletrico: true
 }
 
-console.log(BMW.__proto__);
+const BMW = {
+    modelo: '320i',
+    cor: 'preto',
+    __proto__: CarroLuxo // definindo o objeto que herdaremos
+}
+
+console.log("modelo:", BMW.modelo, ", Ar-Condicionado:", BMW.arCondicionado, ", Velocidade Máxima:", BMW.velMax);
+
+// Herança 2
+
+// cadeia de protótipos (prototype chain)
+
+Object.prototype.Attr0 = 'Zero'
+const avo = { Attr1: 'A' }
+const pai = {__proto__: avo, Attr2: 'B'}
+const filho = {__proto__:pai, Attr3: 'C'}
+console.log(filho.Attr1, filho.Attr0)
