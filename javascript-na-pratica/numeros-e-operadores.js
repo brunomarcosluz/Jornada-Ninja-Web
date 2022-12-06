aac218315341a54ee8800e337c6373b8c9e82efa
Bruno Marcos Luz <brunomarcosluz@gmail.com>
2022-12-06 16:06:31 -0300

    Praticando JS

---
+++ javascript-na-pratica/numeros-e-operadores.js
@@ -0,0 +1,21 @@
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20;
+console.log(total)
+
+// Crie duas expressões que retornem NaN
+let nan1 = 102 + 'kg'
+let nan2  = 'Isso é 100' / 2
+console.log(typeof nan2)
+
+// Somar a string '200' com o número 50 e retornar 250
+let n1 = '200'
+let n2 = 50
+console.log(n1 + n2)
+
+// Incremente o número 5 e retorne o seu valor incrementado
+
+// Como dividir o peso por 2?
+var numero = '80';
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+var pesoPorDois = peso / 2; // NaN (Not a Number)
